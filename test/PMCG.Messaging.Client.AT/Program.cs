1cfd6d53965f220ead92706856d4c0b8918dc102
nmcgrath <nmcgrath@continuumcommerce.com>
2017-10-18 12:20:46 +0100

    Added note in Publisher for issue with automatic recovery and publishing we have encountered

--- test/PMCG.Messaging.Client.AT/Program.cs
+++ test/PMCG.Messaging.Client.AT/Program.cs
@@ -30,8 +30,7 @@ namespace PMCG.Messaging.Client.AT
 			//_publishTests.Publish_1000_Messages_To_A_Queue_Using_Custom_Exchange();
 			//_publishTests.Publish_With_Timeout();
 			//_publishTests.Publish_Connection_Closed_By_Application_Never_Recovers();
-			// TODO Messages disappearing, not expected when running slow??????????
-			//_publishTests.Publish_Connection_Closed_By_Server_Recovers_Automatically();
+			_publishTests.Publish_Connection_Closed_By_Server_Recovers_Automatically();
 			//_publishTests.Publish_Connection_Closed_By_Server_Restart_Unpublished_Messages_Are_Republished_Successfully();
 			//_publishTests.Publish_Connection_Blocked_Then_Unblocked_Unpublished_Messages_Are_Republished_Successfully();
 			//_publishTests.Publish_Invalid_Message_Is_Null();
@@ -46,10 +45,8 @@ namespace PMCG.Messaging.Client.AT
 			//_consumeTests.Publish_10000_Messages_And_Consume_For_The_Same_Messages_With_Ack_Blocked_Then_Unblocked();
 			//_consumeTests.Publish_10000_Messages_And_Consume_For_The_Same_Messages_With_Ack_Connection_Closed_By_Server_Recovers_Automatically();
 			//_consumeTests.Publish_10000_Messages_And_Consume_On_Separate_Bus_For_The_Same_Messages_Consumer_Connection_Closed_By_Server_Recovers_Automatically();
-			_consumeTests.Publish_50000_Messages_And_Then_Consume_On_Separate_Bus_For_The_Same_Messages();
+			//_consumeTests.Publish_50000_Messages_And_Then_Consume_On_Separate_Bus_For_The_Same_Messages();
 			//_consumeTests.Publish_1000_Messages_And_Consume_For_The_Same_Messsage_On_A_Transient_Queue();
-
-			//TODO should we verify consumer counts, also should we update ttl to higher than 10 secs?
 		}
 	}
 }
